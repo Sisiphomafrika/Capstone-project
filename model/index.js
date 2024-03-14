5ef4b9aeb1581f5867bb39edf9813c27f80dadc5
Ccpho <134293167+Sisiphomafrika@users.noreply.github.com>
2024-03-14 15:08:55 +0200

    Update index.js

--- model/index.js
+++ model/index.js
@@ -1,5 +1,5 @@
 import { Users } from './users.js';
-import { Products } from './products.js';
+import { Products } from './Products.js';
 import { Orders } from './Orders.js';
 
 let users = new Users();
